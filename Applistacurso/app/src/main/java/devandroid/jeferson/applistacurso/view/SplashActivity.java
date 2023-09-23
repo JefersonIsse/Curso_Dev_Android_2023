02e59d19ea1260e27b32f796f1b272ca355f8f06
Jeferson_Isse <jeferson.l.marques@gmail.com>
2023-09-23 14:06:40 -0300

    Criando Segunda tela Splash antes da Principal

---
+++ Applistacurso/app/src/main/java/devandroid/jeferson/applistacurso/view/SplashActivity.java
@@ -0,0 +1,38 @@
+package devandroid.jeferson.applistacurso.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import devandroid.jeferson.applistacurso.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    public static final int TIME_OUT_SPLASH =3000; // Tempo que a tela SPLASH
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        cumutarTelaSplash(); // Criou o metodo
+
+    }
+
+    private void cumutarTelaSplash() {   // Oque faz o metodo cumutarTelaSplash
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent telaPrincipal = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(telaPrincipal);
+                finish();
+
+            }
+        },TIME_OUT_SPLASH);
+
+    }
+}
